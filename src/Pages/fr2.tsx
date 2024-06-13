7136a6230a2834914a0fd16f96ee740f6f3f9a0f
nurumohammed <meetnurumd5520@gmail.com>
2024-06-13 20:08:15 +0300

    managing form with react

--- src/Pages/fr2.tsx
+++ src/Pages/fr2.tsx
@@ -1,56 +1,39 @@
-import { FormEvent, useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
 
 const Form2 = () => {
-  const [person, setPerson] = useState({
-    fname: "",
-    lname: "", 
-  });
-
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault();
-    console.log(person);
-  };
-
-  const handleFirstNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPerson({ ...person, fname: e.target.value });
-  };
-
-  const handleLastNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPerson({ ...person, lname: e.target.value });
-  };
+  const { register, handleSubmit } = useForm();
+  const onsubmit = (data: FieldValues) => console.log(data);
 
   return (
     <div className="bg-blue-950 text-white p-4 flex justify-center">
-      <form action="" onSubmit={handleSubmit}>
+      <form action="" onSubmit={handleSubmit(onsubmit)}>
         <p className="text-2xl">Personal Information</p>
         <p className="text-xl pb-10">
           Use a permanent address where you can receive mail
         </p>
         <div>
-          <div className="bg-cyan-300 grid grid-cols-[auto_1fr] gap-10">
+          <div className="bg-cyan- 300 grid grid-cols-[auto_1fr] gap-10">
             <div className="flex flex-col">
               <label htmlFor="" className="text-xl pb-4">
-                First name
+                Name
               </label>
               <input
-                onChange={handleFirstNameChange}
-                value={person.fname}
+                {...register("name")}
                 type="text"
-                id="firstName"
-                name="firstName"
+                id="name"
+                name="name"
                 className="p-2 rounded-md w-96 bg-slate-500/90 hover:border-none shadow-sm shadow-white"
               />
             </div>
             <div className="flex flex-col">
               <label htmlFor="" className="text-xl pb-4">
-                Last name
+                Age
               </label>
               <input
-                onChange={handleLastNameChange}
-                value={person.lname}
-                type="text"
-                id="LastName"
-                name="lastName"
+                {...register("age")}
+                type="number"
+                id="age"
+                name="age"
                 className="p-2 rounded-md w-96 bg-slate-500/90 shadow-sm shadow-white"
               />
             </div>
